d9acca602237563500c2b75dcc6981c1d104c642
vincent van de merwe <335479@student.mboutrecht.nl>
2024-06-25 10:35:37 +0200

    added a klant model and migration because i need them for voedselpakketten table also added a seeder for klanten table and i changed the name of the voedselpakket model to VoedselPakket

---
+++ database/seeders/KlantenSeeder.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KlantenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::tabel('klanten')->insert([
+            'naam' => 'Klant',
+            'aantalvolwassenen' => 3,
+            'aantalkinderen' => 2,
+            'aantalbabies' => 1,
+            'huisnummer' => '21',
+            'postcode' => '3998kx',
+            'plaats' => 'Schalkwijk',
+            'straat' => 'Wierslaan',
+            'voornaam' => 'Jan',
+            'tussenvoegsel' => '',
+            'achternaam' => 'Boom',
+            'email' => 'janboom@hotmail.com',
+        ]);
+
+        DB::tabel('klanten')->insert([
+            'naam' => 'Klant',
+            'aantalvolwassenen' => 2,
+            'aantalkinderen' => 1,
+            'aantalbabies' => 0,
+            'huisnummer' => '15',
+            'postcode' => '3204RF',
+            'plaats' => 'Utrecht',
+            'straat' => 'spronklaan',
+            'voornaam' => 'Piet',
+            'tussenvoegsel' => 'de',
+            'achternaam' => 'Haan',
+            'email' => 'piethaan@outlook.com',
+        ]);
+
+        DB::tabel('klanten')->insert([
+            'naam' => 'Klant',
+            'aantalvolwassenen' => 5,
+            'aantalkinderen' => 7,
+            'aantalbabies' => 3,
+            'huisnummer' => '5',
+            'postcode' => '2958QL',
+            'plaats' => 'Amsterdam',
+            'straat' => 'marinelaan',
+            'voornaam' => 'Klaas',
+            'tussenvoegsel' => 'de',
+            'achternaam' => 'Boer',
+            'email' => 'klaasboer@gmail.com',
+        ]);
+
+        DB::tabel('klanten')->insert([
+            'naam' => 'Klant',
+            'aantalvolwassenen' => 4,
+            'aantalkinderen' => 2,
+            'aantalbabies' => 1,
+            'huisnummer' => '9',
+            'postcode' => '9685ED',
+            'plaats' => 'Groningen',
+            'straat' => 'sprookjeslaan',
+            'voornaam' => 'angela',
+            'tussenvoegsel' => '',
+            'achternaam' => 'stelt',
+            'email' => 'steltangela@gmail.com',
+        ]);
+
+        DB::tabel('klanten')->insert([
+            'naam' => 'Klant',
+            'aantalvolwassenen' => 1,
+            'aantalkinderen' => 5,
+            'aantalbabies' => 3,
+            'huisnummer' => '28',
+            'postcode' => '5847GH',
+            'plaats' => 'Den Haag',
+            'straat' => 'kaasstraat',
+            'voornaam' => 'Ruben',
+            'tussenvoegsel' => 'de',
+            'achternaam' => 'monster',
+            'email' => 'rubenmonster@outlook.com',
+        ]);
+    }
+}
