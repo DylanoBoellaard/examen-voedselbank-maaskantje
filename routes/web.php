349ba424c78d17387ab81507c323c648b4321993
vincent van de merwe <335479@student.mboutrecht.nl>
2024-06-25 12:13:20 +0200

    i added a css folder with voedselpakket.css to the public folder to make it accessable also i added a layout page to have a template and i added 3 pages in voedselpakket folder at last i added a route that connects to the voedselpakketcontroller and to the page overzicht from voedselpakket

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VoedselPakketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/voedselpakket', [VoedselPakketController::class, 'overzicht_voedsel_pakket'])->name('overzicht');
