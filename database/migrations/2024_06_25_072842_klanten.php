fad331d98301037d47830ca30938468ed7a28d6b
stefanoschoonheimMBO <320791@student.mboutrecht.nl>
2024-06-25 09:48:08 +0200

    created klanten tabel migration

---
+++ database/migrations/2024_06_25_072842_klanten.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('klanten', function (Blueprint $table) {
+            $table->id();
+            $table->string('naam');
+            $table->integer('aantalvolwassenen');
+            $table->integer('aantalkinderen');
+            $table->integer('aantalbabies');
+            $table->string('huisnummer');
+            $table->string('postcode');
+            $table->string('plaats');
+            $table->string('straat');
+            $table->string('voornaam');
+            $table->string('tussenvoegsel');
+            $table->string('achternaam');
+            $table->string('email');
+            $table->string('telefoon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
