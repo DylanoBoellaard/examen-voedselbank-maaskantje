10697875fd395c25fa4eded1c7464d8ded8e7a24
stefanoschoonheimMBO <320791@student.mboutrecht.nl>
2024-06-25 09:49:35 +0200

    added engine to klanten migration

--- database/migrations/2024_06_25_072842_klanten.php
+++ database/migrations/2024_06_25_072842_klanten.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Random\Engine;
 
 return new class extends Migration
 {
@@ -28,6 +29,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('telefoon');
             $table->timestamps();
+            $table->engine('InnoDB');
         });
     }
 
