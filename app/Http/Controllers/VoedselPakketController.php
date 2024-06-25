1b77de934f1da5310b02aac9479578ab66244212
vincent van de merwe <335479@student.mboutrecht.nl>
2024-06-25 13:05:59 +0200

    to the VoedselPakketController added data from the database so the data gets send to the page overzicht also added styling for the overzicht page and gave all the styling comments and in the overzicht page i added a table with all the data getting looped through and also have a tr with td with a message that there are no voedsel pakketten on that moment

--- app/Http/Controllers/VoedselPakketController.php
+++ app/Http/Controllers/VoedselPakketController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\VoedselPakket;
 use Illuminate\Http\Request;
 
 class VoedselPakketController extends Controller
 {
     public function overzicht_voedsel_pakket()
     {
-        return view('voedselpakket.overzicht');
+        $data = VoedselPakket::join('klanten', 'voedselpakketten.klant_id', 'klanten.id')->get();
+        return view('voedselpakket.overzicht', compact('data'));
     }
 }
--- resources/views/voedselpakket/overzicht.blade.php
+++ resources/views/voedselpakket/overzicht.blade.php
@@ -1,1 +1,38 @@
 @include("./layouts/app")
+
+<section class="title">
+    <h1>Overzicht voedsel pakketten</h1>
+</section>
+
+<section class="table">
+    <table>
+        <thead>
+            <tr>
+                <th>Datum uitgifte</th>
+                <th>Datum samenstelling</th>
+                <th>Naam</th>
+                <th>Huisnummer</th>
+                <th>Postcode</th>
+                <th>Plaats</th>
+                <th>Straat</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($data as $voedselpakket)
+            <tr>
+                <td>{{date('d-m-Y', strtotime($voedselpakket->datum_uitgifte))}}</td>
+                <td>{{date('d-m-Y', strtotime($voedselpakket->datum_samenstelling))}}</td>
+                <td>{{$voedselpakket->voornaam}}</td>
+                <td>{{$voedselpakket->huisnummer}}</td>
+                <td>{{$voedselpakket->postcode}}</td>
+                <td>{{$voedselpakket->plaats}}</td>
+                <td>{{$voedselpakket->straat}}</td>
+            </tr>
+            @empty
+            <tr>
+                <td colspan="7">er zijn geen voedsel pakketten op dit moment.</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+</section>
