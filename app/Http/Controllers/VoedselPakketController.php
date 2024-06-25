41b21aca83a32e983e414afb031b351188040563
vincent van de merwe <335479@student.mboutrecht.nl>
2024-06-25 14:56:21 +0200

    changed some code from the overzicht function and store function but also added a edit function so that the user can go to the edit form and a update function to update the data in the database with a validation and if the validation is correct then it updates only that data with the same id and after it returns back to the overzicht page

--- app/Http/Controllers/VoedselPakketController.php
+++ app/Http/Controllers/VoedselPakketController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\VoedselPakket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class VoedselPakketController extends Controller
 {
     public function overzicht_voedsel_pakket()
     {
         try {
-            $data = VoedselPakket::join('klanten', 'voedselpakketten.klant_id', 'klanten.id')->get();
+            $data = DB::table('voedselpakketten')->join('klanten', 'voedselpakketten.klant_id', 'klanten.id')->get();
         } catch (\Throwable $e) {
             return redirect(route('overzicht'))->with('alert', 'Error cannot get the data.');
         }
@@ -27,10 +28,28 @@ class VoedselPakketController extends Controller
         $validated = $request->validate([
             'datum_uitgifte' => 'required|date|date_format:Y-m-d',
             'datum_samenstelling' => 'required|date|date_format:Y-m-d',
-            'klant_id' => 'required|integer',
+            'klant_id' => 'required|integer|exists:klanten,id|unique:voedselpakketten,klant_id',
         ]);
 
         VoedselPakket::create($validated);
+
+        return redirect(route('overzicht'));
+    }
+
+    public function edit(VoedselPakket $pakketId)
+    {
+        return view('voedselpakket.edit', compact('pakketId'));
+    }
+
+    public function update(Request $request, VoedselPakket $pakketId)
+    {
+        $validated = $request->validate([
+            'datum_uitgifte' => 'required|date|date_format:Y-m-d',
+            'datum_samenstelling' => 'required|date|date_format:Y-m-d',
+            'klant_id' => 'required|integer|exists:klanten,id|unique:voedselpakketten,klant_id,' . $pakketId->id,
+        ]);
+
+        DB::table('voedselpakketten')->where('voedselpakketten.id', $pakketId->id)->update($validated);
 
         return redirect(route('overzicht'));
     }
