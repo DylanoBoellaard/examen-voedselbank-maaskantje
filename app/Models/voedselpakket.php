c947a2e338e4e5362303ccdda9c0de365e43d3ab
vincent van de merwe <335479@student.mboutrecht.nl>
2024-06-25 09:58:51 +0200

    added a model and a migration for the database, in the model i added a custom protected name for the table and in the migration i made the columns for the table voedselpakketten

---
+++ app/Models/voedselpakket.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class voedselpakket extends Model
+{
+    use HasFactory;
+
+    protected $table = 'voedselpakketten';
+}
